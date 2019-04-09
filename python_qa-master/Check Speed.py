304e7acae6f873d4bb115ad2838fde5551922b3d
Tien.Cao <manhtien.cao@foodycorp.vn>
2019-04-09 11:11:18 +0700

    Tien Cao Excercises

--- python_qa-master/Check Speed.py
+++ python_qa-master/Check Speed.py
@@ -1,12 +1,15 @@
+import math
+
+
 def check_speed(speed):
     if speed <= 70:
         print('Ok!!')
     else:
-        _check_speed = (speed - 70) // 5
+        _check_speed = math.ceil((speed - 70) / 5)
         if _check_speed >= 12:
             print('License suspended')
         else:
-            print('Point: ', _check_speed + 1)
+            print('Point: ', _check_speed)
 
 
 if __name__ == "__main__":
